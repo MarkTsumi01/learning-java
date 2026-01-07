f7742d8f5d06a2e9ddb1d7772df5ce2961982731
MarkTsumi01 <kanraveemark01@gmail.com>
2026-01-07 15:53:24 +0700

    refactor project structures and create example of variable in java

---
+++ src/main/java/com/learning/Variable.java
@@ -0,0 +1,10 @@
+public class Variable{
+	public static void main(String[] args){
+		String a = "a"; //This is String
+		int myNumber1 = 10; //This is Number
+		float floatingNumber = 25.02; //This is Float
+		boolean myBoolean = true; //This is Boolean
+		char myChar = 'a'; //Thisis
+		final myNumber2 = 15; //This final will not allow other overwrite this values
+	}
+}
